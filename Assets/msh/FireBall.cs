d40bedff6795b04ee26e6f1fd5f62117b3211e7f
h0000n <anjunghoon2716@gmail.com>
2023-09-20 16:42:32 +0900

    임시적으로 올립니다.

---
+++ Assets/msh/FireBall.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireBall : MonoBehaviour
+{
+    public Vector3 dir;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += dir * Time.deltaTime * 10;
+        //transform.position = new Vector3(transform.position.x, 0, transform.position.z);
+    }
+}
